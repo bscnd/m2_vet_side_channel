acff2c00f7b6af3b1eb2ae318eaff3030fc9bb26
Tanguy Flégeau <tanguy.flegeau@etudiant.univ-rennes1.fr>
2021-02-18 12:22:57 +0100

    additional corrections, getting indexes for highest person coefficient

--- code/python/pearson.py
+++ code/python/pearson.py
@@ -11,10 +11,14 @@ def compute_keys_models(plaintext, targeted_byte):
             key_model_last[k][q]=model_last_round(plaintext[q], targeted_byte, k)
     return key_model_first, key_model_last
 
-#This function gives the highest pearson coefficient for a given traces and models
+#This function gives the value and indexes for the highest pearson coefficient for a given traces and models
 def get_highest_pearson_coeff(traces, model_first, model_last):
     for k in range(256): #for each key assumption associated to the targeted_byte
         for i in range(len(traces)): #for each sample of traces
-            maxCoeffs_first[k][i]= np.max(abs(np.corrcoef(traces[i], model_first[k])))
-            maxCoeffs_last[k][i]= np.max(abs(np.corrcoef(traces[i], model_last[k])))
-    return maxCoeffs_first, maxCoeffs_last
+            coeffs_first[k][i]= abs(np.corrcoef(traces[i], model_first[k]))
+            coeffs_last[k][i]= abs(np.corrcoef(traces[i], model_last[k]))
+    max_f = np.max(coeffs_first)
+    max_l = np.max(coeffs_last)
+    indexes_f = np.where(coeffs_first == max_f)
+    indexes_l = np.where(coeffs_last == max_l)
+    return max_f, indexes_f, max_l, indexes_l
