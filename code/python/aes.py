4fbca0f4a7a96e10b6d8670881b3dc0d5548c163
bscnd <bscndorian@gmail.com>
2021-02-19 17:21:31 +0100

    Vérification  déchiffrement avec la clé calculée

--- code/python/aes.py
+++ code/python/aes.py
@@ -3,6 +3,7 @@ import os
 import sys
 import numpy as np
 from array import array
+from tqdm import tqdm
 
 s_box = (
     0x63, 0x7C, 0x77, 0x7B, 0xF2, 0x6B, 0x6F, 0xC5, 0x30, 0x01, 0x67, 0x2B, 0xFE, 0xD7, 0xAB, 0x76,
@@ -143,7 +144,7 @@ def leakage_model_first_round(plaintext, key_value, target_byte): # mode 0 Damie
 
 def leakage_model_first_round_allkeys(plaintext, target_byte):
     allkeys_model = [0] * 256
-    for key_value in range (256):
+    for key_value in tqdm(range (256), leave=False, desc = f'byte {target_byte}'):
         allkeys_model [key_value] = leakage_model_first_round(plaintext, key_value, target_byte)
     return allkeys_model
 
--- code/python/enc_test.py
+++ code/python/enc_test.py
@@ -7,15 +7,6 @@ from aes import AES, bytes2matrix, matrix2bytes, sbox_output, leakage_model_firs
 
 def main():
     
-    # iv = os.urandom(16)
-    # master_key = os.urandom(16)
-    # aes_tool = AES(master_key)
-
-    # # Test encryption / decryption
-    # cipher_text = aes_tool.encrypt_block(b'aaaaaaaaaaaaaaaa')
-    # plain_text = aes_tool.decrypt_block(cipher_text)
-    # assert plain_text == b'aaaaaaaaaaaaaaaa'
-
     # # Expand key
     # all_keys = AES._expand_key(aes_tool, master_key)
     # print(str(all_keys) + "\n")                     # Affiche toutes les clés
@@ -40,18 +31,15 @@ def main():
     # master_key=bytes2matrix(master_key)
     # print(str(master_key) + "\n")
 
-    # # Test sbox_output
-    # plaintext = b'aaaaaaaaaaaaaaaa'
-    # master_key = b'aaaaaaaaaaaaaaaa'
-    # n_octet = 0
-    # sbox = sbox_output(plaintext[n_octet], master_key[n_octet])  # En décimal, à passer en hexa pour retrouver les valeurs de la table (99 = 0x63)
-    # print("Sortie de la SBox au premier tour d'AES pour l'octet n° " + str(n_octet) + " : " + str(sbox))
 
     #Load data
-    plaintext = np.load("F:\Documents\Cours\m2_cyber\m2_vet\data\software_traces_k_known\\plaintext.npy")
-    traces = np.load("F:\Documents\Cours\m2_cyber\m2_vet\data\software_traces_k_known\\traces.npy")
-    known_key = np.load("F:\Documents\Cours\m2_cyber\m2_vet\data\software_traces_k_known\\key.npy")
-    computed_key = np.zeros(16)
+    plaintext = np.load("F:\Documents\Cours\m2_cyber\m2_vet\data\software_traces_k_unknown\\plaintext.npy")
+    ciphertext = np.load("F:\Documents\Cours\m2_cyber\m2_vet\data\software_traces_k_unknown\\ciphertext.npy")
+    traces = np.load("F:\Documents\Cours\m2_cyber\m2_vet\data\software_traces_k_unknown\\traces.npy")
+    traces = traces [:2000, :]
+
+    #known_key = np.load("F:\Documents\Cours\m2_cyber\m2_vet\data\software_traces_k_known\\key.npy")
+    computed_key = np.zeros(16, dtype=np.uint8)
 
     fig, axs= plt.subplots(1)
     for i in range(16):
@@ -59,13 +47,18 @@ def main():
         model_first_round = leakage_model_first_round_allkeys(plaintext, i)
         correlation_values, max_f, indexes_f = get_highest_pearson_coeff(traces, model_first_round)
         
-        computed_key[i] = indexes_f[1]
+        computed_key[i] = np.uint8(indexes_f[1])
 
         # Display highest correlation value
         axs.plot(correlation_values[indexes_f[0]])
 
-    plt.show()
-    
+    # Display all the highest correlation values
+    # plt.show()
+
+    # key = [123, 86, 39, 250, 142, 4, 139, 87, 144, 205, 225, 221, 217, 24, 29, 31]
+    aes = AES (bytes(computed_key))
+    assert np.all(bytes (plaintext [:, 0]) == aes.decrypt_block (bytes (ciphertext [:, 0])))
+
     
 if __name__ == "__main__":
     main()
